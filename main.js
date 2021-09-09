13c8cad453b765327118dd1c380a6ad67d93afa6
tonni276 <tanniakhter123@gmail.com>
2021-09-09 14:45:20 +0600

    first commit

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function submitIssue(e) {
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
+  const id = Math.floor(Math.random() * 100000000) + '';
   const status = 'Open';
 
   const issue = { id, description, severity, assignedTo, status };
   let issues = [];
-  if (localStorage.getItem('issues')){
+  if (localStorage.getItem('issues')) {
     issues = JSON.parse(localStorage.getItem('issues'));
   }
   issues.push(issue);
@@ -23,34 +23,34 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => issue.id == id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
-
-const deleteIssue = id => {
+  const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  // console.log(issues);
+    const remainingIssues = issues.filter(issue => issue.id != id);
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
-}
+  fetchIssues();
+  }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
-
   for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
-
-    issuesList.innerHTML +=   `<div class="well">
-                              <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-                              </div>`;
+    const { id, description, severity, assignedTo, status } = issues[i];
+    const mark =`<del>${description}</del>`
+    issuesList.innerHTML += `<div class="well">
+       <h6>Issue ID: ${id} </h6>
+        <p><span class="label label-info"> ${status} </span></p>
+        <h3>${(status !== 'Closed')?description:mark}</h3>
+        <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+        <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+        <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+        <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+        </div>`;
   }
 }
